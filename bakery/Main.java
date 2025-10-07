f02eae4ec2e017d7b164f753d8b510050500b934
bluinha <cecyliahelenna@gmail.com>
2025-10-07 13:07:44 -0300

    Pedido de Bolo alterado no main para que multiplos tipos de bolos fossem pedidos.

--- bakery/Main.java
+++ bakery/Main.java
@@ -5,12 +5,11 @@ public class Main {
     public static void main(String[] args) {
         // Create the order
         Order order = new Order();
-        order.addCake(new VanillaCake());
+
         order.addCake(new ChocolateCake());
-        order.addCake(new MultiLayeredCake( new ChocolateCake()));
-        order.addCake(new MultiLayeredCake( new SprinklesCake(new VanillaCake())));
-        order.addCake(new WithSayingCake(new VanillaCake(), "one of") );
-        order.addCake(new StrawberryCake());
+        order.addCake(new WithSayingCake(new VanillaCake(), "PLAIN!") );
+        order.addCake( new SprinklesCake(new WithSayingCake(new VanillaCake(),"FANCY")));
+        order.addCake(new MultiLayeredCake( new SprinklesCake(new WithSayingCake(new WithSayingCake(new StrawberryCake(), "One of"), "EVERYTHING"))));
 
         // Print the order
         order.printOrder();
